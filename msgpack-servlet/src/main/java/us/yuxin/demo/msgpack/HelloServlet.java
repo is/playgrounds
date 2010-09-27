dab295934b775e1d0da4ced9dee5c6f00f95ca6b
Yu Xin <scaner@gmail.com>
2010-09-27 21:34:15 +0800

    msgpack-servlet: project go on...
     - add jetty-maven-plugin in pom.xml
     - add webapp & web.xml
     - add test HelloServlet

---
+++ msgpack-servlet/src/main/java/us/yuxin/demo/msgpack/HelloServlet.java
@@ -0,0 +1,23 @@
+package us.yuxin.demo.msgpack;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class HelloServlet extends HttpServlet {
+	private static final long serialVersionUID = -6598627353024514611L;
+
+	@Override
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp)
+			throws ServletException, IOException {
+		resp.setContentType("text/html;Charset=utf8");
+		PrintWriter writer = resp.getWriter();
+		writer.println("<html><head><title>Hello World</title></head>");
+		writer.println("<body><h1>Hi</h1></body></html>");
+		writer.close();
+	}
+}
