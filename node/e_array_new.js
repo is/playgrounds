9508880c60d55f39a13866212b3437134d9eb8a3
Yu Xin <scaner@gmail.com>
2010-10-20 00:25:03 +0800

    node: more object test in e_array_new.js

--- node/e_array_new.js
+++ node/e_array_new.js
@@ -5,3 +5,22 @@ a.push('b');
 
 console.log(a);
 console.log(a.length);
+
+
+console.log('--object-0--');
+b = Object();
+b.hello = 0;
+console.log(b.hello);
+console.log(b.hello2);
+
+if (b['hello']) {
+	console.log('b.hello');
+}
+
+if (b['hello'] != undefined) {
+	console.log('b.hello-defined');
+}
+
+if (b['hello2']) {
+	console.log('b.hello2');
+}
