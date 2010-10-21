9d736f832eb0289855580c52b7c47f5d64592e1d
Yu Xin <scaner@gmail.com>
2010-10-21 19:07:12 +0800

    node: rumor, first work version

--- node/rumor.js
+++ node/rumor.js
@@ -3,7 +3,11 @@ var events = require('events'),
     sys = require('sys'),
     url = require('url'),
 		urlparse = require('url').parse,
+		htmlparser = require('htmlparser'),
 		BufferList = require('bufferlist').BufferList;
+
+var cllog = console.log
+var clinfo = console.log
 
 // ---- Configuration ----
 var C = {
@@ -25,16 +29,19 @@ function Entry(url, referer) {
 		this.port = ui.port;
 		this.hostname = ui.hostname;
 		this.protocol = ui.protocol;
-		this.path = ui.path;
+		this.pathname = ui.pathname;
 
-		if (this.path === undefined)
-			this.path = '/';
+		if (this.pathname === undefined)
+			this.pathname = '/';
 		if (this.port === undefined)
-			this.port = 80;
+			if (this.protocol === 'http:')
+				this.port = 80;
+			else if (this.protocol === 'https:')
+				this.port = 443;
 		return this;
-	}
+	};
 	this.parse();
-}
+};
 
 // ---- Rumor ----
 function Rumor(C) {
@@ -42,18 +49,23 @@ function Rumor(C) {
 
 	this.conf = C;
 	this.entries = new Array;
+	this.entriesMap = new Object;
+
+	this.entriesLimit1 = 2500;
+	this.entriesLimit2 = 5000;
+
 	this.sites = new Object;
 	this.tasks = new Object;
 
 	this.curFetcherId = 0;
 	this.curFetchers = 0;
-	this.curFetcherLimit = 10000;
+	this.curFetcherLimit = 2600;
 
 	// ----
 	this.genFetcherId = function() {
 		this.curFetcherId ++;
 		return this.curFetcherId;
-	}
+	};
 
 	// ----
 	this.fireFetcher = function(entry) {
@@ -61,7 +73,33 @@ function Rumor(C) {
 		this.addFetcher(fetcher);
 		fetcher.fire();
 		return fetcher;
-	}
+	};
+
+	this.fireFetchers = function() {
+		while (true) {
+			if (this.entries.length == 0)
+				return;
+
+			if (this.curFetchers >= this.curFetcherLimit)
+				return;
+
+			var ei = this.entries.shift();
+			delete this.entriesMap[ei.url];
+			if (this.sites[ei.hostname] && this.sites[ei.hostname] > 8)
+				continue;
+
+			this.fireFetcher(ei);
+		}
+	};
+
+	// ----
+	this.addEntry = function(entry) {
+		if (this.entriesMap[entry.url] !== undefined)
+			return;
+
+		this.entriesMap[entry.url] = entry;
+		this.entries.push(entry);
+	};
 
 	// ----
 	this.addFetcher = function(task) {
@@ -75,11 +113,13 @@ function Rumor(C) {
 		var sites = this.sites;
 		if (sites[task.entry.host] != undefined)
 			sites[task.entry.host] += 1;
-		else
+		else {
+			cllog("[site] " + task.entry.host +  " <= " + task.entry.referer + " | " + this.curFetcherId + " | " + this.entries.length + " | " + this.curFetchers);
 			sites[task.entry.host] = 1;
+		}
 
 		task.registered = true;
-	}
+	};
 
 	// ----
 	this.removeFetcher = function(task) {
@@ -98,27 +138,21 @@ function Rumor(C) {
 		}
 			
 		task.registered = false;
-	}
+	};
 
 	this.onFetcherFinished = function(fetcher) {
-		// console.log('Fetcher Finished');
-		// console.log(fetcher.retCode);
-		if (fetcher.content == undefined) {
-			console.log("   fetched " + fetcher.entry.url + " (" + fetcher.retCode + ")");
-		} else {
-			console.log(fetcher.content.length);
-		}
-	}
+		this.planner.onFetcherFinished(fetcher);
+	};
 
 	// ----
 	this.version = function() {
 		return C.version;
-	}
+	};
 
 	this.tagline = function() { 
-		console.log(" = Rumor -- " + this.version());
-	}
-}
+		clinfo("Rumor -- " + this.version());
+	};
+};
 sys.inherits(Rumor, events.EventEmitter);
 
 // ---- Fetcher ----
@@ -136,13 +170,19 @@ function Fetcher(R, ei) {
 	this.fire = function() {
 		var self = this;
 		this.co = http.createClient(this.entry.port, this.entry.hostname);
+		this.co.on('error', function(exception) {
+			self.clear();
+			self.co.destroy();
+		});
+
 		var headers = {};
 		headers.host = this.entry.host;
+		headers.agent = 'rumor-' + this.rumor.version();
 
 		if (this.entry.referer)
 			headers.referer = this.entry.referer;
 
-		this.request = this.co.request('GET', this.entry.path, headers);
+		this.request = this.co.request('GET', this.entry.pathname, headers);
 		this.request.on('response', function(response) { 
 			self.onResponse(response);
 		});
@@ -150,13 +190,12 @@ function Fetcher(R, ei) {
 			self.onRequestError(exception)
 		});
 		this.request.end();
-	}
+	};
 
 	this.onResponse = function(response) {
 		var self = this;
 		this.response = response;
 		this.retCode = response.statusCode;
-		this.resHeaders = response.headers;
 
 		if (this.retCode != 200) {
 			this.finished();
@@ -167,7 +206,7 @@ function Fetcher(R, ei) {
 		response.on('data', function(chunk) { self.onResponseData(chunk); }).
 			on('error', function(exception) { self.onResponseError(exception); }).
 			on('end', function() { self.onResponseEnd(); });
-	}
+	};
 
 	this.onResponseData = function(chunk) {
 		this.content.push(chunk);
@@ -176,35 +215,172 @@ function Fetcher(R, ei) {
 				this.onResponseError('too-big-contents');
 			}
 		}
-	}
+	};
 
 	this.onRequestError = function(error) {
 		this.clear();
 		this.co.destroy();
-	}
+	};
 
 	this.onResponseError = function(error) {
 		this.clear();
 		this.response.client.destroy();
-	}
+	};
 
 	this.onResponseEnd = function() {
 		this.finished();
-	}
+	};
 
 	this.clear = function() {
 		this.rumor.removeFetcher(this);
-	}
+	};
 
 	this.finished = function() {
 		this.clear();
 		this.rumor.onFetcherFinished(this);
-	}
+	};
+};
+
+// ---- 
+function Planner(rumor) {
+	this.rumor = rumor;
+	this.onFetcherFinished = function(fetcher) {
+		var retCode = fetcher.retCode;
+		if (retCode == 301 || retCode == 302 || retCode == 307) {
+			this.onRedirect(fetcher);
+			return;
+		}
+
+		if (retCode == 200) {
+			this.onContent(fetcher);
+			return;
+		}
+		
+		cllog("[" + fetcher.retCode + "] " + fetcher.entry.url);
+		return;
+	};
+
+	this.onContent = function(fetcher) {
+		if (!fetcher.content && !fetcher.content.length) {
+			// cllog("[200] " + fetcher.entry.url + " {empty}");
+			return;
+		}
+
+		var contentType = fetcher.response.headers['content-type'];
+		var contentLength = fetcher.response.headers['content-length'];
+
+		if (!contentType)
+			contentType = "-";
+
+		if (!contentLength)
+			contentLength = fetcher.content.length;
+
+		// if (fetcher.entry.referer)
+		//	if (fetcher.entry.host !== urlparse(fetcher.entry.referer).host)
+		//		cllog(" vvv  " + fetcher.entry.referer);
+	
+		var accu = rumor.entries.length;
+		if (accu >= rumor.entriesLimit2) {
+			// cllog("[200] " + fetcher.entry.url + " === " + contentType + ":" + contentLength + " | " + rumor.curFetcherId + " | " + rumor.entries.length + " | " + rumor.curFetchers);
+			return;
+		}
+
+		var self = this;
+		var handler = new htmlparser.DefaultHandler();
+		var parser = new htmlparser.Parser(handler);
+		try {
+			parser.parseComplete(fetcher.content);
+		} catch(e) {
+			return;
+		}
+		
+		linktags = htmlparser.DomUtils.getElementsByTagName('a', handler.dom);
+		var entries = [];
+		var entriesMap = {};
+
+		for (var i = 0; i < linktags.length; i++) {
+			var l = linktags[i];
+			if (!l.attribs || !l.attribs.href)
+				continue;
+			href = l.attribs.href;
+			var u = url.resolve(fetcher.entry.url, href);
+			var e = new Entry(u, fetcher.entry.url);
+			if (e.protocol === 'http:') {
+				if (!entriesMap[u]) {
+					entries.push(e);
+					entriesMap[u] = 1;
+				}
+			}
+		}
+
+		var added = entries.length
+		if (accu >= rumor.entriesLimit1 && added > 30) {
+			added = 25;
+		}
+
+		var swapper = function(a, L, e) {
+			var r = Math.floor(Math.random() * L);
+			var x = a[e];
+			a[e] = a[r];
+			a[r] = a[e];
+		};
+
+		for (var i = 0, L = entries.length; i < added; i++)
+			swapper(entries, L, i);
+
+		for (var i = 0; i < added; i++) {
+			if (entries[i].protocol === "http:")
+				rumor.addEntry(entries[i]);
+		}
+
+		// cllog("[200] " + fetcher.entry.url + " === " + contentType + ":" + contentLength + " | " + added + " links | " + rumor.curFetcherId + " | " + rumor.entries.length + " | " + rumor.curFetchers);
+		rumor.fireFetchers();
+	};
+
+	this.onRedirect = function(fetcher) {
+		var redirectURL = fetcher.response.headers['location'];
+		if (!redirectURL)
+			return;
+
+		redirectURL = url.resolve(fetcher.entry.url, redirectURL);
+		// cllog("[" + fetcher.retCode + "] " + fetcher.entry.url + " -> " + redirectURL);
+		this.addEntry(redirectURL, fetcher.referer, true);
+	};
+
+
+	this.addEntry = function(url, referer, fire) {
+		var e = new Entry(url, referer);
+		if (e.protocol === 'http:') {
+			rumor.addEntry(e);
+		}
+		if (fire) {
+			rumor.fireFetchers();
+		}
+	};
 }
 
-
 // ---- Main ----
-var r = new Rumor(C);
-r.tagline();
-var ei = new Entry('http://www.sun.com/');
-r.fireFetcher(ei);
+function main() {
+/*
+	cl.setInfoPrefix(' = ');
+	cl.setLogPrefix(' . ');
+	cl.setErrorPrefix(' ! ');
+	cl.setWarnPrefix(' + ');
+
+	cl.setLogColor('BLUE');
+	cl.setInfoColor('MAGENTA');
+	cl.setErrorColor('RED');
+	cl.setWarnColor('RED');
+*/
+	var r = new Rumor(C);
+	r.planner = new Planner(r);
+	r.tagline();
+	var ei = new Entry('http://linuxtoy.org/archives/ubuntu_and_qt.html');
+	r.fireFetcher(ei);
+}
+
+main();
+
+process.on('exit', function() {
+	console.log('--- END ---');
+});
