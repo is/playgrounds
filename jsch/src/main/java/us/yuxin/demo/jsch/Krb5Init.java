5b1a5430a049b1e16d45360662219f1392985d7b
Xin Yu <scaner@gmail.com>
2013-02-15 00:33:34 +0800

    Add callback handler.

--- jsch/src/main/java/us/yuxin/demo/jsch/Krb5Init.java
+++ jsch/src/main/java/us/yuxin/demo/jsch/Krb5Init.java
@@ -1,12 +1,16 @@
 package us.yuxin.demo.jsch;
 
 
+import javax.security.auth.callback.CallbackHandler;
 import javax.security.auth.login.LoginContext;
 import javax.security.auth.login.LoginException;
 
+import com.sun.security.auth.callback.TextCallbackHandler;
+
 public class Krb5Init {
-  public static void main(String args[]) throws LoginException {
-    LoginContext lc = new LoginContext(Krb5Init.class.getName());
+  public static void main(String args[]) throws LoginException
+  {
+    LoginContext lc = new LoginContext(Krb5Init.class.getName(), new TextCallbackHandler());
     lc.login();
   }
 }
