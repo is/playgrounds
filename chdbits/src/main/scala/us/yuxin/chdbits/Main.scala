13f983018f0971c29bc8984b8f0900a61e79ff17
Xin Yu <scaner@gmail.com>
2013-03-19 17:51:04 +0800

    Get rid of useless code.

--- chdbits/src/main/scala/us/yuxin/chdbits/Main.scala
+++ chdbits/src/main/scala/us/yuxin/chdbits/Main.scala
@@ -7,9 +7,6 @@ import java.io.{FileOutputStream, File}
  * @author ${user.name}
  */
 object Main {
-
-  def foo(x: Array[String]) = x.foldLeft("")((a, b) => a + b)
-
   def main(args: Array[String]) {
     val config = ConfigFactory.parseFile(new File("application.json"))
 
