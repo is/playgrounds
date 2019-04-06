0df055bcde3f43f5d5536ec9066b1d74a2253ef4
Xin Yu <scaner@gmail.com>
2019-04-06 14:11:27 +0800

    try share subscription. #mqtt

---
+++ mqtt-0/m0_share.py
@@ -0,0 +1,39 @@
+import sys
+import time
+import paho.mqtt.client as paho
+
+from __cf import C
+
+#broker = "iot.eclipse.org"
+broker = C.broker
+topic = "house/bulb1"
+client_id = "mai-0"
+
+if len(sys.argv) > 1:
+  client_id = sys.argv[1]
+
+def on_message(client, userdata, message):
+  print('received message =', str(message.payload.decode('utf-8')))
+
+def on_connect(client, userdata, flags, rc):
+    print("Connected with result code "+str(rc))
+
+client = paho.Client(client_id)
+client.on_message = on_message
+client.on_connect = on_connect
+
+if ('app_id' in C):
+  client.username_pw_set(username=C.app_id, password=C.secret)
+
+print("connecting to broker:%s" % broker)
+client.connect(broker)
+client.loop_start()
+client.subscribe("$queue/" + topic)
+time.sleep(2)
+print("sending...")
+for i in range(200):
+  client.publish(topic, "on-%d-from-%s" % (i, client_id))
+print("out...")
+time.sleep(100)
+client.disconnect()
+client.loop_stop()
