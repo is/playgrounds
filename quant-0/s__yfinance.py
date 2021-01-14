95efb47f1687c89c524e45cccbc2f88dfd613214
Yu Xin <scaner@gmail.com>
2021-01-15 01:29:19 +0800

    . [quant-0] update.

--- quant-0/s__yfinance.py
+++ quant-0/s__yfinance.py
@@ -1,4 +1,8 @@
 # https://github.com/ranaroussi/yfinance
 # pip install yfinance --upgrade --no-cache-dir
 
+import yfinance as yf
 
+data = yf.download(tickers='AAPL',
+    period='5d', interval='1m', auto_adjust = True)
+print(data)
