d09b598793a4aa096d59bc3129e8794e207c1aa8
Xin Yu <scaner@gmail.com>
2021-03-16 00:36:13 +0800

    + [cv-1] clip source, avoid tree shadow.

--- cv-1/s04_mask_0.py
+++ cv-1/s04_mask_0.py
@@ -5,21 +5,39 @@ import numpy as np
 fn = 'star-cache/gray__x4/s_00.png'
 
 def main():
-    gray = cv2.imread(fn, cv2.IMREAD_GRAYSCALE)
-    gray0 = gray
-    gray = cv2.GaussianBlur(gray, (7, 7), 0)
+    src = cv2.imread(fn, cv2.IMREAD_GRAYSCALE)
+    gray = src
+    
+    gaussian = cv2.GaussianBlur(gray, (5, 5), 0)
+    cv2.imwrite("s/02__mask_blur_gaussian.png", gaussian)
+    print(f"gaussian mean:{np.mean(gaussian)}, std:{np.std(gaussian)}")
+
+    bilateral = cv2.bilateralFilter(src, 7, 42, 70)
+    cv2.imwrite("s/02__mask_bulr_bilateral.png", bilateral)
+    print(f"bilateral mean:{np.mean(bilateral)}, std:{np.std(bilateral)}")
+
+    median = cv2.medianBlur(src,5)
+    cv2.imwrite("s/02__mask_blur_median.png", median)
+    print(f"median mean:{np.mean(median)}, std:{np.std(median)}")
+
+
+    gray = gaussian
+
     mean = np.mean(gray)
     std = np.std(gray)
     print(f"mean:{mean}, std:{std}")
-    gray2 = np.abs(gray - int(mean))
-    ret0, mask0 = cv2.threshold(gray, mean + int(std * 1.5), 255, cv2.THRESH_BINARY)
+
+    ret0, mask0 = cv2.threshold(gray, mean + int(std * 2.15), 255, cv2.THRESH_BINARY)
     ret1, mask1 = cv2.threshold(gray, mean - int(std * 2), 255, cv2.THRESH_BINARY_INV)
     mask2 = mask0 + mask1
-    cv2.imwrite("s/02__mask_src.png", gray0)
-    cv2.imwrite("s/02__mask_blur.png", gray)
-    cv2.imwrite("s/02__mask_0.png", mask0)
+    mask2 = cv2.medianBlur(mask2, 3)
+
+    cv2.imwrite("s/02__mask_src.png", src)
+    cv2.imwrite("s/02__mask_0.png", cv2.GaussianBlur(255 - mask0, (5, 5), 0))
     cv2.imwrite("s/02__mask_1.png", mask1)
     cv2.imwrite("s/02__mask_2.png", mask2)
+
+
 
 
 if __name__ == '__main__':
